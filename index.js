9fff37ae8efca940c6bb5af7afbbd3214a68ce05
az1zbek2004 <xoldarovazizbek04@gmail.com>
2023-12-23 22:52:20 +0500

    Masalalar yechildi

---
+++ index.js
@@ -0,0 +1,88 @@
+// FIRST EXEMPLE 
+// let arr = [21, 3, 5, 90, 3, 11, 1];
+
+// const indexSearch = (arg) => {
+// }
+
+// console.log(indexSearch(arr));
+
+
+// SECOND EXEMPLE 
+// let arr1 = [1,2,3];
+// let arr2 = [4,5,6];
+// let arr3 = [7,8,9];
+
+// function add(prm1, prm2) {
+//     let mass = prm2.concat(prm1, prm1);
+//     return mass;
+// }
+
+// console.log(add(arr1, arr2));
+
+
+// THIRD EXEMPLE 
+// let arr = [21, 3, 5, 90, 3, 11, 1];
+// const kvSon = (arg) => {
+//     let a=arg.find((value) => {
+//         return value<11;
+//     });
+    
+//     return a**2;
+// }
+
+// console.log(kvSon(arr));
+
+// FOURTH EXEMPLE 
+// let arr = [21, 3, 5, 90, 3, 11, 1];
+
+// const toqSon = function(arg){
+//     let counter=0;
+//     arg.forEach((value) => {
+    
+//     if (value%2==1) {
+//         counter++;
+//     }
+
+//  });
+
+//  return counter;
+// }
+
+//  console.log(toqSon(arr));
+
+// FIFTH EXEMPLE 
+// let wordArray=['salom', 'hello', 'xayr', 'bye', 'world', 'wide', 'web' ];
+// function words(arg) {
+//     let res=[];
+//     arg.forEach((value) => {
+//     res.push(value.charAt(value.length-1));
+// });
+// return res;
+// }
+
+// console.log(words(wordArray));
+
+// SIX EXEMPLE 
+// let arr = [3, 11, 21, 32, 41, 13];
+
+// const tubSonmi = function(prm) {
+//     let counter=0;
+//     let sum=0;
+//     for (let i = 0; i <= prm.length; i++) {
+//        let counter1=0;
+//       for (let j = 0; j <= prm[i]; j++) {
+//         if (prm[i]%j==0) {
+//             counter1++;
+//         }        
+//       }
+
+//       if (counter1==2) {
+//         counter++;
+//         sum+=prm[i];
+//       }
+//     }
+
+//     return sum;
+// }
+
+// console.log(tubSonmi(arr));
